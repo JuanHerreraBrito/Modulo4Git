2abc3325839221e3d1e676dd86e34ba97de4316d
JUAN HERRERA <juanherrera@MacBook-Air-de-JUAN.local>
2023-06-20 00:55:52 -0600

    Initial commit

---
+++ app/src/main/java/com/darooma/modulo4/ejercicios/CicloVidaToastActivity.kt
@@ -0,0 +1,51 @@
+package com.darooma.modulo4.ejercicios
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.darooma.modulo4.R
+
+class CicloVidaToastActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.e("CICLO DE VIDA", "OnCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "Metodo OnStart", Toast.LENGTH_SHORT).show()
+        Log.e("CICLO DE VIDA", "OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "Metodo OnResume", Toast.LENGTH_SHORT).show()
+        Log.e("CICLO DE VIDA", "OnResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "Metodo OnPause", Toast.LENGTH_SHORT).show()
+        Log.e("CICLO DE VIDA", "OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "Metodo OnStop", Toast.LENGTH_SHORT).show()
+        Log.e("CICLO DE VIDA", "OnStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this, "Metodo OnRestart", Toast.LENGTH_SHORT).show()
+        Log.e("CICLO DE VIDA", "OnRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "Metodo OnDestroy", Toast.LENGTH_SHORT).show()
+        Log.e("CICLO DE VIDA", "OnDestroy")
+    }
+}
---
+++ app/src/main/java/com/darooma/modulo4/ejercicios/IntentActivity.kt
@@ -0,0 +1,12 @@
+package com.darooma.modulo4.ejercicios
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.darooma.modulo4.R
+
+class IntentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intent)
+    }
+}
