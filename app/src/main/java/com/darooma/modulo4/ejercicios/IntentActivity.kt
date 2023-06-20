e5c2316bdaf3138d5d5032b4344dfde9d4568871
juanherrera <JuanHerreraBrito>
2023-06-20 13:26:42 -0600

    Se actualiza Intent ejercicio de abrir un correo y una pagina web

--- app/src/main/java/com/darooma/modulo4/ejercicios/IntentActivity.kt
+++ app/src/main/java/com/darooma/modulo4/ejercicios/IntentActivity.kt
@@ -1,12 +1,32 @@
 package com.darooma.modulo4.ejercicios
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.darooma.modulo4.R
 
 class IntentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent)
+
+        val btSend = findViewById<Button>(R.id.btsend)
+        val btUrl = findViewById<Button>(R.id.btUrl)
+
+        btSend.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto")
+                putExtra(Intent.EXTRA_SUBJECT, "aviso urgente")
+                putExtra(Intent.EXTRA_TEXT, "Esto es una prueba para enviar correo medinte intent implicito")
+            }
+            startActivity(Intent.createChooser(emailIntent, "Enviar email usando:"))
+        }
+
+        btUrl.setOnClickListener {
+            val urlIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://google.com"))
+            startActivity(urlIntent)
+        }
     }
 }
